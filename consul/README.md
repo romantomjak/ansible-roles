d8081f829b168ef1155be3132b44bad1ec0916a5
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2022-01-15 21:12:58 +0000

    Upgrade Consul to 1.11 (#15)

--- consul/README.md
+++ consul/README.md
@@ -1,38 +1,61 @@
 # consul
 
-Example playbook for deploying Consul nodes with default ACL policy of "deny".
+Example playbook for deploying Consul agents.
 
 ---
 
-Consul clients can be deployed with the same configuration - just change `consul_server` to say `false`.
+Consul clients can be deployed with the same configuration - just change `consul_agent_type` to say `client`.
 
 ```yml
 ---
-# Deploys Consul node
+# Deploys Consul agent
 
-- name: Provision Consul node
+- name: Provision Consul agent
   hosts: all
   vars:
-    consul_server: true
-    consul_encrypt: Luj2FZWwlt8475wD1WtwUQ==
+    consul_agent_type: server
+    consul_datacenter: dc1
+    consul_bootstrap_expect: 3
+    consul_encrypt: qDOPBEr+/oUVeOFQOnVypxwDaHzLrD+lvjo5vCEBbZ0=
     consul_retry_join: ["172.16.0.11"]
     consul_acl_master_token: b1gs33cr3t
     consul_acl_agent_token: fe3b8d40-0ee0-8783-6cc2-ab1aa9bb16c1
-    consul_advertise_bind: eth0
+    consul_tls_ca_cert: -----BEGIN CERTIFICATE...
+    consul_tls_ca_key: -----BEGIN EC PRIVATE KEY...
   roles:
     - consul
 ```
 
+By default a TLS enabled consul cluster will be deployed.
+
 More configuration options and explanations in the [defaults/main.yml](/consul/defaults/main.yml)
 
-## Agent Token policy
+## Gossip encryption key
 
-The `acl_agent_token` is a special token that is used for an agent's internal operations. It isn't used directly for any user-initiated operations.
+Gossip is encrypted with a symmetric key, since gossip between nodes is done over UDP. All agents must have the same encryption key.
 
-The ACL agent token is used for the following operations by the agent:
+You can generate the key before starting Consul, as long as you have the Consul binary installed in your path:
 
-- Updating the agent's node entry using the Catalog API, including updating its node metadata, tagged addresses, and network coordinates
-- Performing anti-entropy syncing, in particular reading the node metadata and services registered with the catalog
+```shell
+$ consul keygen
+qDOPBEr+/oUVeOFQOnVypxwDaHzLrD+lvjo5vCEBbZ0=
+```
+
+## Initializing the built-in CA
+
+One of the first steps to configuring TLS for Consul is generating certificates. In order to prevent unauthorized datacenter access, Consul requires all certificates be signed by the same Certificate Authority (CA). This should be a private CA and not a public one as any certificate signed by this CA will be allowed to communicate with the datacenter.
+
+You will only need to create one CA for the datacenter. You can create the CA and certificates before starting Consul, as long as you have the Consul binary installed in your path.
+
+```shell
+$ consul tls ca create
+==> Saved consul-agent-ca.pem
+==> Saved consul-agent-ca-key.pem
+```
+
+The CA certificate, consul-agent-ca.pem, contains the public key necessary to validate Consul certificates and therefore must be distributed to every node that runs a consul agent.
+
+The CA key, consul-agent-ca-key.pem, will be used to sign certificates for Consul nodes and must be kept private. Possession of this key allows anyone to run Consul as a trusted server or generate new valid certificates for the datacenter and obtain access to all Consul data, including ACL tokens.
 
 ## Anonymous Token policy
 
---
+++ consul/library/consul_acl_token.py
@@ -0,0 +1,197 @@
+import json
+from copy import copy
+from dataclasses import dataclass
+from typing import Dict, List
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.urls import fetch_url
+
+
+@dataclass
+class Configuration:
+    accessor_id: str
+    secret_id: str
+    description: str
+    policies: List[Dict]
+    roles: List[Dict]
+    local: bool
+    state: str
+    scheme: str
+    host: str
+    port: int
+    mgmt_token: str
+
+
+@dataclass
+class Token:
+    accessor_id: str
+    secret_id: str
+    description: str
+    policies: List[Dict]
+    roles: List[Dict]
+    local: bool
+
+    def to_json(self):
+        obj = copy(self.__dict__)
+        obj["accessorid"] = obj.pop("accessor_id", "")
+        obj["secretid"] = obj.pop("secret_id", "")
+        return json.dumps(obj, default=lambda o: o.__dict__)
+
+    @classmethod
+    def from_dict(cls, obj):
+        return Token(
+            accessor_id=obj.get("AccessorID"),
+            secret_id=obj.get("SecretID"),
+            description=obj.get("Description"),
+            policies=obj.get("Policies"),
+            roles=obj.get("Roles"),
+            local=obj.get("Local"),
+        )
+
+
+@dataclass
+class Output:
+    token: Token
+    changed: bool
+
+
+def set_token(module: AnsibleModule, configuration: Configuration):
+    json_resp = request(module, "GET", "acl/tokens", configuration.mgmt_token, scheme=configuration.scheme, host=configuration.host, port=configuration.port)
+
+    for obj in json_resp:
+        token = Token.from_dict(obj)
+
+        if configuration.accessor_id and configuration.accessor_id == token.accessor_id:
+            return update_token(module, configuration, token.accessor_id)
+
+        if configuration.secret_id and configuration.secret_id == token.secret_id:
+            return update_token(module, configuration, token.accessor_id)
+
+    return create_token(module, configuration)
+
+
+def update_token(module: AnsibleModule, configuration: Configuration, accessor_id: str):
+    json_resp = request(module, "GET", f"acl/token/{accessor_id}", configuration.mgmt_token, scheme=configuration.scheme, host=configuration.host, port=configuration.port)
+    existing_token = Token.from_dict(json_resp)
+
+    configured_token = Token(
+        accessor_id=accessor_id,  # if the ID was not specified it will be resolved at this point
+        secret_id=configuration.secret_id,
+        description=configuration.description,
+        policies=configuration.policies,
+        roles=configuration.roles,
+        local=configuration.local,
+    )
+
+    if existing_token == configured_token:
+        return Output(changed=False, token=existing_token)
+
+    request(module, "PUT", f"acl/token/{accessor_id}", configuration.mgmt_token, data=configured_token.to_json(), scheme=configuration.scheme, host=configuration.host, port=configuration.port)
+
+    return Output(changed=True, token=configured_token)
+
+
+def create_token(module: AnsibleModule, configuration: Configuration):
+    configured_token = Token(
+        accessor_id=configuration.accessor_id,
+        secret_id=configuration.secret_id,
+        description=configuration.description,
+        policies=configuration.policies,
+        roles=configuration.roles,
+        local=configuration.local,
+    )
+
+    json_resp = request(module, "PUT", "acl/token", configuration.mgmt_token, data=configured_token.to_json(), scheme=configuration.scheme, host=configuration.host, port=configuration.port)
+
+    return Output(changed=True, token=Token.from_dict(json_resp))
+
+
+def remove_token(module: AnsibleModule, configuration: Configuration):
+    return Output(changed=True)
+
+
+def request(module, method, endpoint, token, data=None, scheme="http", host="localhost", port=8500):
+    url = f"{scheme}://{host}:{port}/v1/{endpoint}"
+    headers = {
+        "Content-Type": "application/json",
+        "X-Consul-Token": token,
+    }
+
+    resp, info = fetch_url(module, url, method=method, data=data, headers=headers)
+
+    if info.get("status") != 200:
+        module.fail_json(
+            msg=f"Consul API error: {info.get('msg')}",
+            method=method,
+            url=url,
+            status_code=info.get("status"),
+            response=info.get("body"),
+        )
+
+    return json.loads(resp.read())
+
+
+def main():
+    module_args = dict(
+        accessor_id=dict(default=""),
+        secret_id=dict(default=""),
+        description=dict(default=""),
+        policies=dict(type="list", elements="dict"),
+        roles=dict(type="list", elements="dict"),
+        local=dict(type="bool", default=False),
+        state=dict(choices=["present", "absent"], default="present"),
+        scheme=dict(default="http"),
+        host=dict(default="localhost"),
+        port=dict(type="int", default=8500),
+        mgmt_token=dict(required=True, no_log=True),
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=False
+    )
+
+    result = dict(
+        changed=False,
+    )
+
+    configuration = Configuration(
+        accessor_id=module.params.get("accessor_id"),
+        secret_id=module.params.get("secret_id"),
+        description=module.params.get("description"),
+        policies=module.params.get("policies"),
+        roles=module.params.get("roles"),
+        local=module.params.get("local"),
+        state=module.params.get("state"),
+        scheme=module.params.get("scheme"),
+        host=module.params.get("host"),
+        port=module.params.get("port"),
+        mgmt_token=module.params.get("mgmt_token"),
+    )
+
+    try:
+        if configuration.state == "present":
+            output = set_token(module, configuration)
+        else:
+            output = remove_token(module, configuration)
+    except ConnectionError as e:
+        module.fail_json(
+            msg="Failed to connect to Consul agent at {scheme}://{host}:{port}: {error}".format(
+                scheme=configuration.scheme,
+                host=configuration.host,
+                port=configuration.port,
+                error=str(e),
+            ),
+            **result
+        )
+
+    result["changed"] = output.changed
+
+    if output.token is not None:
+        result["token"] = output.token.__dict__
+
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
